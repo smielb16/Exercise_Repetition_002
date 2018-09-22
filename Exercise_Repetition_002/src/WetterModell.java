e0208202bc1f9836a7c80dd5afae603c26d5d1b2
Friedrich Gutmensch <smielb16@htlkaindorf.at>
2018-09-22 17:15:04 +0200

    designed GUI

---
+++ Exercise_Repetition_002/src/WetterModell.java
@@ -0,0 +1,17 @@
+
+import javax.swing.AbstractListModel;
+
+
+public class WetterModell extends AbstractListModel{
+
+    @Override
+    public int getSize() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ Exercise_Repetition_002/src/WetterWert.java
@@ -0,0 +1,11 @@
+
+import java.util.Date;
+
+
+public class WetterWert {
+    private int temperatur;
+    private int luftfeuchtigkeit;
+    private Date Zeitpunkt;
+    
+    
+}
